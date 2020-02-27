e2701835dbe76471d79a3152d6685d9a088e05ea
Rotimi Kirya <okirya@ucsd.edu>
2020-02-27 10:18:01 -0800

    Minor change

--- routes/rsvp.js
+++ routes/rsvp.js
@@ -10,8 +10,8 @@ exports.adminView = function(request, response){
 };
 
 exports.addRSVP = function(request, response) {
-var rsvpEmail = request.body.rsvpEmail;
-console.log(rsvpEmail);
-data.rsvp.push(rsvpEmail);
-response.send(rsvpEmail);
-}
+	var rsvpEmail = request.body.rsvpEmail;
+	console.log(rsvpEmail);
+	data.rsvp.push(rsvpEmail);
+	response.send(rsvpEmail);
+};
